e0d8df6d4c6a01e4832ef870275b475b0d93cc35
waleed <waleed.sukhon77@gmail.com>
2023-07-08 07:35:23 +0300

    global popup build

--- src/components/PopUp/ResistrationPopUp/RegistrationPopUs.tsx
+++ src/components/PopUp/ResistrationPopUp/RegistrationPopUs.tsx
@@ -3,10 +3,9 @@ import RegistrationForm from "../../RegistrationForm.tsx/RegistrationForm";
 import P from "../../SimpleComponents/P";
 import PopUp from "../PopUp";
 
-export default ({ setShow }: { setShow: any }) => (
+export default () => (
   <PopUp
     submitButton={<MainButton text={"dawwda"} />}
-    setShow={setShow}
     form={<RegistrationForm />}
     title="تسجيل حساب جديد"
     extra={
